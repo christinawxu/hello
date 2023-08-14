e9dea8b7c3e8a287d078cb2f2b9ff53aab8a7da0
christinawxu <142261727+christinawxu@users.noreply.github.com>
2023-08-14 14:52:59 -0500

    initial commit

---
+++ hello.c
@@ -0,0 +1,17 @@
+/**
+ * Author: Christina Xu
+ *         cxu24@huskers.unl.edu
+ * Date: 2023/08/14
+ *
+ * A simple hello world program in C
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Christina Xu\n");
+
+  return 0;
+}
